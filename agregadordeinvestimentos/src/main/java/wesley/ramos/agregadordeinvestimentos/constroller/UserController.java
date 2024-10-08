5a6fb5203fafae7af3aeda3af71679806c47ef8d
Wesley <lomesramos@gmail.com>
2024-10-08 15:10:51 -0300

    feat:  create constroller

---
+++ agregadordeinvestimentos/src/main/java/wesley/ramos/agregadordeinvestimentos/constroller/UserController.java
@@ -0,0 +1,61 @@
+package wesley.ramos.agregadordeinvestimentos.constroller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import wesley.ramos.agregadordeinvestimentos.dto.CreateUserDTO;
+import wesley.ramos.agregadordeinvestimentos.dto.UpdateUserDTO;
+import wesley.ramos.agregadordeinvestimentos.entity.User;
+import wesley.ramos.agregadordeinvestimentos.service.UserService;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping("/v1/users")
+
+public class UserController {
+
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody CreateUserDTO createUserDTO){
+        var userId = userService.createUser(createUserDTO);
+        return ResponseEntity.created(URI.create("/v1/users/" + userId.toString())).build();
+    }
+    @GetMapping("/{userId}")
+    public ResponseEntity<User> getUserById(@PathVariable("userId")String userId){
+    var user = userService.getUserById(userId);
+    if(user.isPresent()){
+        return ResponseEntity.ok(user.get());
+    } else {
+        return ResponseEntity.notFound().build();
+    }
+
+    }
+    @GetMapping
+    public ResponseEntity<List<User>> listUsers(){
+    var users = userService.listUsers();
+    return ResponseEntity.ok(users);
+    }
+
+    @PutMapping("/{userId}")
+    public ResponseEntity<Void> updateUserById(@PathVariable("userId")String userId,
+                                               @RequestBody UpdateUserDTO updateUserDTO){
+        userService.updateUserById(userId, updateUserDTO);
+        return ResponseEntity.noContent().build();
+
+    }
+
+    @DeleteMapping("/{userId}")
+    public ResponseEntity<Void> deleteById(@PathVariable("userId")String userId){
+        userService.deleteById(userId);
+        return ResponseEntity.noContent().build();
+
+        }
+
+
+}
