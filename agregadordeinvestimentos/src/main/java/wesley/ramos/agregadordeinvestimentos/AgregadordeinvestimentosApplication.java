0970682f079e2101f156b435d1a781bf25375d46
Wesley <lomesramos@gmail.com>
2024-10-08 15:08:10 -0300

    feat: initial config commit

---
+++ agregadordeinvestimentos/src/main/java/wesley/ramos/agregadordeinvestimentos/AgregadordeinvestimentosApplication.java
@@ -0,0 +1,13 @@
+package wesley.ramos.agregadordeinvestimentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgregadordeinvestimentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgregadordeinvestimentosApplication.class, args);
+	}
+
+}
---
+++ agregadordeinvestimentos/src/test/java/wesley/ramos/agregadordeinvestimentos/AgregadordeinvestimentosApplicationTests.java
@@ -0,0 +1,13 @@
+package wesley.ramos.agregadordeinvestimentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgregadordeinvestimentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
